d1656a950e736d9b0418ffebb3644f82448d1a4e
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-17 12:06:45 +0200

    uddate fourth exercise

---
+++ 4. python_sort_by_key_json.py
@@ -0,0 +1,24 @@
+import json
+
+
+def dictionary():
+
+    key_value = {"2":20, "1": 2, "5": 12, "4": 20, "6": 10}
+
+    print(f"Original key_value {key_value}")
+
+    sorted_obj = dict(sorted(key_value.items(), key=lambda keyval: (keyval[0], keyval[1])))
+
+    print(f"Sorted by key value: {json.dumps(sorted_obj)}")
+
+
+def main():
+    dictionary()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
